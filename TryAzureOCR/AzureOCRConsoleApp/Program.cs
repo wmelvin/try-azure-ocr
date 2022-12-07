45caaede916d228ca6665649587751e7244ca832
Bill Melvin <bill@billmelvin.com>
2022-12-06 20:34:43 -0500

    Use Configuration Extensions to get settings.

--- TryAzureOCR/AzureOCRConsoleApp/Program.cs
+++ TryAzureOCR/AzureOCRConsoleApp/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace AzureOCRConsoleApp
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AzureOCRConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Settings settings = GetSettings();
+
+
             Console.WriteLine("Hello, World!");
+        }
+        private static Settings GetSettings()
+        {
+            string configFile = Path.Join(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            "KeepLocal", "cogfunc-config.json");
+
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine($"ERROR: Cannot find {configFile}");
+                Environment.Exit(1);
+            }
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile(configFile)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Settings? settings = config.GetRequiredSection("Settings").Get<Settings>();
+            if (settings is null)
+            {
+                Console.WriteLine($"ERROR: Cannot get Settings from {configFile}");
+                Environment.Exit(1);
+            }
+
+            if (settings.Key is null)
+            {
+                Console.WriteLine("ERROR: Cannot get 'Key' setting.");
+                Environment.Exit(1);
+            }
+
+            if (settings.Endpoint is null)
+            {
+                Console.WriteLine("ERROR: Cannot get 'Endpoint' setting.");
+                Environment.Exit(1);
+            }
+
+            return settings;
         }
     }
 }
---
+++ TryAzureOCR/AzureOCRConsoleApp/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureOCRConsoleApp
+{
+    public sealed class Settings
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Endpoint { get; set; } = string.Empty;
+    }
+}
