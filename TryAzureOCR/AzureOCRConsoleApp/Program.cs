d517b523d2e5d7792364e82a583ce948c90b1c0a
Bill Melvin <bill@billmelvin.com>
2022-12-06 20:24:58 -0500

    Initial commit. New console app.

---
+++ TryAzureOCR/AzureOCRConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureOCRConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
